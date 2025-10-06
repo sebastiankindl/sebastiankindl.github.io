8f2163fa0160dce6f01d9bb6232d98517a00a908
sebastiankindl <sebastian.kindl11@gmail.com>
2025-10-06 20:12:15 +0200

    password removed

--- password.js
+++
@@ -1,7 +0,0 @@
-const password = "hallohallo1010"; // dein Passwort hier
-const userInput = prompt("Password:");
-
-if (userInput !== password) {
-  document.body.innerHTML = "<h2 style='text-align:center;margin-top:50px;'>Zugriff verweigert 🚫</h2>";
-  throw new Error("Zugriff verweigert");
-}
